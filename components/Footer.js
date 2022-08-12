8a88e5e5ec959b947b6edd4b08cc75e5892e3b61
gyanAsh <gyanash36@protonmail.com>
2022-08-12 18:00:15 +0530

    Added new Layout components

---
+++ components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>This is supposed to be a footer.</div>
+  )
+}
+
+export default Footer
---
+++ components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className="flex items-center justify-center w-full h-20 text-white bg-sky-900">
+      Where were you ?
+    </div>
+  )
+}
+
+export default Header
